5bdcc23af9c82f38de7013d33b8dd18486de91c8
Joseph Bergenstråhle <joseph.bergenstrahle@gmail.com>
2019-06-12 16:46:33 +0200

    added pixel coords to prep from table

--- R/STUtility.R
+++ R/STUtility.R
@@ -8,106 +8,6 @@
 #' @importFrom Seurat CreateSeuratObject
 "_PACKAGE"
 
-#' load count files
-#'
-#' @param path Path to count files
-#' @param suffix add suffix to path
-#' @param row.names set column as row.names
-#' @keywords internal
-
-st.load.matrix = function(path, suffix="", row.names=1, ...) {
-  x = c()
-  tmp = try({ x = read.delim(paste(path, suffix, sep=""),
-                             header=T,
-                             row.names=row.names,
-                             sep=",",
-                             check.names=F,
-                             ...)})
-  if(inherits(tmp, 'try-error')) {
-    return(as.matrix(c()))
-  } else {
-    return(as.matrix(x))
-  }
-}
-
-#' Create S4 object
-#'
-#' @param sampleTable table with paths to count files and metadata
-#' @param transpose set TRUE if count files are in the form of genes as columns and spatial
-#' coordiantes as rows.
-#' @param topN OPTIONAL: Filter out the top most expressed genes
-#' @param th.gene OPTIONAL: Filter genes with total counts across all samples below threshold
-#' @param th.spot OPTIONAL: Filter `"capture-spots"` with total count values below threshold
-#' @export
-
-prepare.counts <- function(sampleTable, transpose=TRUE,
-                           topN=0, th.gene=0, th.spot=0,
-                           object.type = "Seurat", ...){
-  outObj = NULL
-  counts <- list()
-  paths <- sampleTable[,1]
-  for(path in paths) {
-    cat(paste0("Loading ", path, "\n"))
-    if(transpose==FALSE){
-      counts[[path]] <- st.load.matrix(path)
-    }else{counts[[path]] <- t(st.load.matrix(path))}
-
-    #pre-filter
-    counts[[path]] <- counts[[path]][rowSums(counts[[path]])>th.gene, colSums(counts[[path]])>th.spot]
-
-  }
-
-  genes <- c()
-  for(count in counts)
-    genes <- union(genes, rownames(count))
-
-  samples <- c()
-  cnt <- matrix(0, nrow=length(genes), ncol=0)
-  rownames(cnt) <- genes
-
-
-  idx <- 1
-  for(count in counts) {
-    nspots <- ncol(count)
-    curgenes <- rownames(count)
-    m <- matrix(0, nrow=length(genes), ncol=nspots)
-    rownames(m) <- genes
-    m[curgenes,] <- count
-    colnames(m) <- paste(colnames(count), "_", idx, sep="")
-    cnt <- cbind(cnt, m)
-    samples <- c(samples, rep(idx, nspots))
-    idx <- idx + 1
-  }
-
-  if(object.type=="Seurat"){
-    m <- CreateSeuratObject(counts = cnt)
-    m$sample <- samples
-    m <- m[order(rowSums(as.matrix(m[["RNA"]]@counts)), decreasing=TRUE),]
-  }else{
-    m <- SingleCellExperiment(assays=list(counts=cnt))
-    m$sample <- samples
-    m <- m[order(rowSums(assays(m)$counts), decreasing=TRUE),]
-  }
-    if (topN > 0){
-    m <- m[1:topN, ]
-    }
-
-
-  #Att metadata
-  if(!is.null(ncol(sampleTable)) & ncol(sampleTable)>1){
-    for(column in colnames(sampleTable[2:ncol(sampleTable)])){
-      #print(column)
-      m[[column]] <- sampleTable[samples, column]
-    }
-  }
-
-
-  cat(paste("After filtering the dimensions of the experiment is: "))
-  print(dim(m))
-
-  return(m)
-
-}
 
 #' Palette selection
 #'
@@ -228,10 +128,10 @@ seh.plot <- function(se, filename, bg="black", col.scale.cluster=TRUE, disable.v
 
 #' Create tSNE based on spatial expression histology
 #'
-#' @param se
-#' @param perpelxity
-#' @param priorPCA
-#' @param bg
+#' @param se s4 object
+#' @param perpelxity perplexity paramter tSNE
+#' @param priorPCA use PCA as input to tSNE
+#' @param bg plot background color
 #' @export
 
 seh.tsne <- function(se, filename, perplexity=30, priorPCA=FALSE, bg="black"){
@@ -290,20 +190,22 @@ seh.tsne <- function(se, filename, perplexity=30, priorPCA=FALSE, bg="black"){
 
 #' Spatial Expression Histology
 #'
-#' @param se
-#' @param n.clust
-#' @param sample.wise.z
-#' @param global.z
-#' @param m.var
-#' @param log.freq
-#' @param use.cov
-#' @param n.start
-#' @param m.iter
+#' @param se s4 object
+#' @param n.clust number of clusters
+#' @param sample.wise.z sample wize z-score calculation
+#' @param global.z global z-score calculation
+#' @param m.var variance?
+#' @param log.freq use log of freq
+#' @param use.cov use covarance
+#' @param n.start number of starts kMeans
+#' @param m.iter number of iterations kMeans
 #' @export
 
 runSEH <- function(se, n.clust=10, sample.wise.z = FALSE, global.z = FALSE, m.var=0,
                    log.freq=TRUE, use.cov = TRUE, n.start=10, m.iter=10,
                    db.scan = FALSE, db.sca.eps = 0.15, db.scan.MinPts = 5) {
+
+  # --------------- OBS OBS ----- TAnk pa TOP genes EJ SORTED BY DEFAULT HAR
 
   if(sample.wise.z){
     useCov=FALSE
@@ -404,8 +306,8 @@ runSEH <- function(se, n.clust=10, sample.wise.z = FALSE, global.z = FALSE, m.va
 #'
 #' Creates tables of the gene clusters, both with raw count data,
 #' and the relative freq. data used in the heatmap plots.
-#' @param se
-#' @param filename
+#' @param se s4 object
+#' @param filename filename
 #' @export
 
 save.table <- function(se, filename){
