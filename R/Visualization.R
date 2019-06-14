8a6125e8dcdcbc854d6c7d9afdff3409afa16ea9
ludvigla <ludvig.larsson@scilifelab.se>
2019-06-14 16:25:25 +0200

    made small changes to prepare.counts function so that sample variable is character

--- R/Visualization.R
+++ R/Visualization.R
@@ -7,7 +7,7 @@
 #' Graphs the output of a dimensional reduction technique on a 2D scatter plot where each point is a
 #' spot and it's positioned based on the ST array coordinates in a grid pattern. Spots are colored
 #' by the dimensional reduction vectors specified by the user.
-#' 
+#'
 #' Function built upon the DimPlot() function from Seurat (https://github.com/satijalab/seurat/blob/master/R/utilities.R)
 #'
 #' @param object Seurat object
@@ -22,9 +22,10 @@
 #' @param palette color palette to use for heatmap, see \code{\link{palette.select}}
 #' @param rev.cols logical specifying whether colorscale should be reversed
 #' @param combine Combine plots into a single gg object; note that if TRUE; themeing will not work when plotting multiple features
-#' @param ncol Number of columns for display when combining plots
+#' @param ncol Number of columns for display of samples for each dimensionality reduction vector
 #' @param delim delimiter passed to \code{\link{GetCoords}} if adjusted ST coordinates are missing in the meta data
-#' @param return.plot.list should the plots be returned as a list? By default, each dimension is plotted separately in a for loop
+#' @param return.plot.list should the plots be returned as a list? By default, the plots are arranged into a grid
+#' @param grid.ncol Number of columns for display when combining plots
 #' @param ... Extra parameters passed on to \code{\link{STPlot}}
 #'
 #' @return A ggplot object
@@ -35,20 +36,21 @@
 #' @export
 
 ST.DimPlot <- function(
-  object, 
-  dims = c(1, 2), 
-  spots = NULL, 
-  pt.size = 1, 
-  reduction = NULL, 
-  group.by = NULL, 
-  shape.by = NULL, 
+  object,
+  dims = c(1, 2),
+  spots = NULL,
+  pt.size = 1,
+  reduction = NULL,
+  group.by = NULL,
+  shape.by = NULL,
   palette = "MaYl",
   rev.cols = F,
   dark.theme = F,
-  combine = TRUE, 
-  ncol = NULL, 
+  combine = TRUE,
+  ncol = NULL,
   delim = NULL,
   return.plot.list = F,
+  grid.ncol = NULL,
   ...
 ) {
   reduction <- reduction %||% {
@@ -70,12 +72,12 @@ ST.DimPlot <- function(
       group.by <- "sample"
       data[, group.by] <- object[[group.by, drop = TRUE]]
     }
-  } 
-  
+  }
+
   if (!is.null(x = shape.by)) {
     data[, shape.by] <- object[[shape.by, drop = TRUE]]
   }
-  
+
   # Obtain array coordinates
   if (all(c("adj_x", "adj_y") %in% colnames(object[[]]))) {
     data <- cbind(data, setNames(object[[c("adj_x", "adj_y")]], nm = c("x", "y")))
@@ -86,58 +88,58 @@ ST.DimPlot <- function(
     coords <- GetCoords(colnames(object), delim)
     data <- cbind(data, coords[, c("x", "y")])
   }
-  
+
   # Create plots
   plots <- lapply(X = dims, FUN = function(d) {
-    plot <- STPlot(data, 
-                   group.by, 
-                   d, 
-                   pt.size, 
-                   palette, 
+    plot <- STPlot(data,
+                   group.by,
+                   d,
+                   pt.size,
+                   palette,
                    rev.cols,
                    ncol,
                    ...)
-    
+
     if (dark.theme) {
       plot <- plot + dark_theme()
     }
     return(plot)
   })
-  
+
   if (return.plot.list) {
     return(plots)
   } else {
-    cowplot::plot_grid(plotlist = plots)
+    cowplot::plot_grid(plotlist = plots, ncol = grid.ncol)
   }
-} 
+}
 
 
 #' Graphs ST spots colored by continuous variable, e.g. dimensional reduction vector
-#' 
+#'
 #' @param data data.frame containing (x, y) coordinates, a group vector and a continuous variable vector
 #' @param group.by specifies column to facet the plots by, e.g. sampleID
 #' @param variable name of continuous variable
 #' @param pt.size point size of each ST spot
 #' @param palette color palette used for spatial heatmap
 #' @param rev.cols logical specifying whether colorscale should be reversed
-#' @param ... parameters passed to geom_point() 
+#' @param ... parameters passed to geom_point()
 #' @export
 
 STPlot <- function(
-  data, 
-  group.by, 
-  variable, 
+  data,
+  group.by,
+  variable,
   pt.size = 1,
   palette = "MaYl",
   rev.cols = F,
-  ncol = NULL, 
+  ncol = NULL,
   ...
 ) {
-  cols <- palette.select(palette)(50)
+  cols <- palette.select(palette)(3)
   if (rev.cols) {
     cols <- rev(cols)
   }
-  ggplot() +
+  p <- ggplot() +
       geom_point(data = data, mapping = aes_string(x = "x", y = "64 - y", color = variable), size = pt.size, ...) +
       scale_x_continuous(limits = c(0, 67)) +
       scale_y_continuous(limits = c(0, 64)) +
@@ -146,14 +148,14 @@ STPlot <- function(
       labs(title = variable, color = "") +
       scale_color_gradientn(colours = cols)
 }
-  
+
 
 #' Generates a dark theme for STPlot
 
 dark_theme <- function() {
-  theme(plot.background = element_rect(fill = "black"), 
+  theme(plot.background = element_rect(fill = "black"),
         plot.title = element_text(colour = "white"),
         strip.text = element_text(colour = 'white'),
-        legend.title = element_text(colour = "white"), 
+        legend.title = element_text(colour = "white"),
         legend.text = element_text(colour = "white"))
 }
