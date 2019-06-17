5139da12af5d2a6193402aea3ea8b6aea9ec11a7
Ludvig Larsson <ludviglarsson@LudvigLaMBP.local>
2019-06-17 17:17:13 +0200

    Fixed visualization functions

--- R/Visualization.R
+++ R/Visualization.R
@@ -13,6 +13,8 @@
 #' @param object Seurat object
 #' @param dims Dimensions to plot, must numeric vectoir specifying number of dimensions to plot
 #' @param spots Vector of spots to plot (default is all spots)
+#' @param min.cutoff,max.cutoff Vector of minimum and maximum cutoff values for each feature,
+#'  may specify quantile in the form of 'q##' where '##' is the quantile (eg, 'q1', 'q10')
 #' @param pt.size Adjust point size for plotting
 #' @param reduction Which dimensionality reduction to use. If not specified, first searches for umap, then tsne, then pca
 #' @param group.by Name of a metadata column to facet plot by (deault is sampleID)
@@ -21,7 +23,6 @@
 #' different colors and different shapes on cells
 #' @param palette color palette to use for heatmap, see \code{\link{palette.select}}
 #' @param rev.cols logical specifying whether colorscale should be reversed
-#' @param combine Combine plots into a single gg object; note that if TRUE; themeing will not work when plotting multiple features
 #' @param ncol Number of columns for display of samples for each dimensionality reduction vector
 #' @param delim delimiter passed to \code{\link{GetCoords}} if adjusted ST coordinates are missing in the meta data
 #' @param return.plot.list should the plots be returned as a list? By default, the plots are arranged into a grid
@@ -40,6 +41,8 @@ ST.DimPlot <- function(
   object,
   dims = c(1, 2),
   spots = NULL,
+  min.cutoff = NA,
+  max.cutoff = NA,
   pt.size = 1,
   reduction = NULL,
   group.by = NULL,
@@ -47,7 +50,6 @@ ST.DimPlot <- function(
   palette = "MaYl",
   rev.cols = F,
   dark.theme = F,
-  combine = TRUE,
   ncol = NULL,
   delim = NULL,
   return.plot.list = F,
@@ -65,6 +67,12 @@ ST.DimPlot <- function(
   data <- as.data.frame(x = data)
   dims <- paste0(Key(object = object[[reduction]]), dims)
 
+  # Select colorscale
+  palette.info <- palette.select(info = T)
+  palette <- palette %||% {
+    palette <- subset(palette.info, category == "div")$palette[1]
+  }
+
   if (!is.null(x = group.by)) {
     data[,  group.by] <- object[[group.by, drop = TRUE]]
   } else if ("sample" %in% colnames(object[[]])) {
@@ -76,7 +84,10 @@ ST.DimPlot <- function(
   }
 
   if (!is.null(x = shape.by)) {
-    data[, shape.by] <- object[[shape.by, drop = TRUE]]
+    data[, shape.by] <- as.character(object[[shape.by, drop = TRUE]])
+    features <- colnames(x = data)[1:(ncol(x = data) - 4)]
+  } else {
+    features <- colnames(x = data)[1:(ncol(x = data) - 3)]
   }
 
   # Obtain array coordinates
@@ -90,16 +101,19 @@ ST.DimPlot <- function(
     data <- cbind(data, coords[, c("x", "y")])
   }
 
+  data <- feature.scaler(data, features, min.cutoff, max.cutoff, spots)
+
   # Create plots
   plots <- lapply(X = dims, FUN = function(d) {
     plot <- STPlot(data,
+                   data.type = "numeric",
                    group.by,
+                   shape.by,
                    d,
                    pt.size,
                    palette,
                    rev.cols,
-                   ncol,
-                   ...)
+                   ncol)
 
     if (dark.theme) {
       plot <- plot + dark_theme()
@@ -167,7 +181,7 @@ ST.FeaturePlot <- function(
   pt.size = 1,
   group.by = NULL,
   shape.by = NULL,
-  palette = "MaYl",
+  palette = NULL,
   rev.cols = F,
   dark.theme = F,
   ncol = NULL,
@@ -180,12 +194,30 @@ ST.FeaturePlot <- function(
   data <- FetchData(object = object, vars = c(features), cells = spots, slot = slot)
   data <- as.data.frame(lapply(data, function(x) {
     new_x <- ifelse(test = sapply(x, function(n) {class(n) == "factor"}), yes = as.character(x), no = x)
-    new_x <- ifelse(test = sapply(x, function(n) {class(n) == "integer"}), yes = as.numeric(x), no = x)
     return(new_x)
   }))
 
-  if (length(unique(sapply(data, class))) > 1) {
+  data.type <- unique(sapply(data, class))
+
+  if (length(data.type) > 1) {
     stop("Mixed classes (", paste(unique(sapply(data, class)), collapse = ", "), ") are not allowed in features ... ")
+  }
+
+  # Select colorscale
+  if (!is.null(palette)) {
+    if (data.type == "numeric") {
+      stopifnot(palette %in% subset(palette.info, category == "div")$palette)
+    } else if (data.type == "integer") {
+      stopifnot(palette %in% subset(palette.info, category == "seq")$palette)
+    }
+  }
+  palette.info <- palette.select(info = T)
+  palette <- palette %||% {
+    if (data.type == "numeric") {
+      palette <- subset(palette.info, category == "div")$palette[1]
+    } else if (data.type == "integer") {
+      palette <- subset(palette.info, category == "seq")$palette[1]
+    }
   }
 
   if (!is.null(x = group.by)) {
@@ -199,6 +231,7 @@ ST.FeaturePlot <- function(
   }
 
   if (!is.null(x = shape.by)) {
+    stopifnot(shape.by %in% colnames(object[[]]))
     data[, shape.by] <- object[[shape.by, drop = TRUE]]
   }
 
@@ -221,15 +254,16 @@ ST.FeaturePlot <- function(
          call. = FALSE)
   }
 
-
   if (class(unique(sapply(data, class))) == "numeric") {
-    data <- feature.scaler(data, min.cutoff, max.cutoff)
+    data <- feature.scaler(data, min.cutoff, max.cutoff, spots)
   }
 
   # Create plots
   plots <- lapply(X = features, FUN = function(d) {
     plot <- STPlot(data,
+                   data.type,
                    group.by,
+                   shape.by,
                    d,
                    pt.size,
                    palette,
@@ -254,10 +288,9 @@ ST.FeaturePlot <- function(
 #'
 #' @param data data.frame containing (x, y) coordinates, a group vector and a continuous variable vector
 #' Graphs ST spots colored by numeric variable, e.g. dimensional reduction vector or a character
-#'
-#' @param data data.frame containing (x, y) coordinates, a group vector and feature vectors of class
-#' numeric/character
+#' @param data.type type of data, e.g. numeric or integer
 #' @param group.by specifies column to facet the plots by, e.g. sampleID
+#' @param shape.by specifies column to shape points by, e.g. morphological region
 #' @param variable name of continuous variable
 #' @param pt.size point size of each ST spot
 #' @param palette color palette used for spatial heatmap
@@ -267,7 +300,9 @@ ST.FeaturePlot <- function(
 
 STPlot <- function(
   data,
+  data.type,
   group.by,
+  shape.by,
   variable,
   pt.size = 1,
   palette = "MaYl",
@@ -279,22 +314,31 @@ STPlot <- function(
   if (rev.cols) {
     cols <- rev(cols)
   }
-  p <- ggplot() +
-      geom_point(data = data, mapping = aes_string(x = "x", y = "64 - y", color = variable), size = pt.size, ...) +
+
+  p <- ggplot()
+  if (!is.null(shape.by)) {
+    p <- p + geom_point(data = data, mapping = aes_string(x = "x", y = "64 - y", color = variable, shape = shape.by), size = pt.size)
+  } else {
+    p <- p + geom_point(data = data, mapping = aes_string(x = "x", y = "64 - y", color = variable), size = pt.size)
+  }
+  p <- p +
       scale_x_continuous(limits = c(0, 67)) +
       scale_y_continuous(limits = c(0, 64)) +
       theme_void() +
       facet_wrap(as.formula(paste("~", group.by)), ncol = ncol) +
-      labs(title = variable, color = "") +
-  if (class(data[, variable, drop = T]) == "numeric") {
+      labs(title = variable, color = "")
+  if (data.type == "numeric") {
     cols <- palette.select(palette)(3)
     if (rev.cols) {
       cols <- rev(cols)
     }
     p <- p +
       labs(title = variable, color = "value") +
+      scale_color_gradient2(low = cols[1], mid = cols[2], high = cols[3], midpoint = 0)
+  } else if (data.type == "integer") {
+    p <- p + labs(title = variable, color = "value") +
       scale_color_gradientn(colours = cols)
-  } else if (class(data[, variable, drop = T]) %in% c("character", "factor")) {
+  } else if (data.type %in% c("character", "factor")) {
     p <- p +
       labs(color = variable)
   }
@@ -344,11 +388,12 @@ SetQuantile <- function(
 
 feature.scaler <- function(
   data,
+  features,
   min.cutoff,
-  max.cutoff
+  max.cutoff,
+  spots
 ) {
-  features <- colnames(x = data)[1:(ncol(x = data) - 3)]
-  min.cutoff <- mapply(
+  min.cutoff <- setNames(mapply(
     FUN = function(cutoff, feature) {
       return(ifelse(
         test = is.na(x = cutoff),
@@ -358,8 +403,8 @@ feature.scaler <- function(
     },
     cutoff = min.cutoff,
     feature = features
-  )
-  max.cutoff <- mapply(
+  ), nm = features)
+  max.cutoff <- setNames(mapply(
     FUN = function(cutoff, feature) {
       return(ifelse(
         test = is.na(x = cutoff),
@@ -369,7 +414,7 @@ feature.scaler <- function(
     },
     cutoff = max.cutoff,
     feature = features
-  )
+  ), nm = features)
   check.lengths <- unique(x = vapply(
     X = list(features, min.cutoff, max.cutoff),
     FUN = length,
@@ -379,7 +424,7 @@ feature.scaler <- function(
     stop("There must be the same number of minimum and maximum cuttoffs as there are features")
   }
 
-  data[, 1:(ncol(x = data) - 2)] <- sapply(X = 1:(ncol(x = data) - 2),
+  data[, features] <- sapply(X = features,
                                            FUN = function(index) {
                                              data.feature <- as.vector(x = data[, index])
                                              min.use <- SetQuantile(cutoff = min.cutoff[index], data.feature)
@@ -388,8 +433,6 @@ feature.scaler <- function(
                                              data.feature[data.feature > max.use] <- max.use
                                              return(data.feature)
                                            })
-  colnames(x = data)[1:(ncol(x = data) - 3)] <- features
-  rownames(x = data) <- spots
   return(data)
 }
 
