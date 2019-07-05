6189f689be2cb676fc8885575e506aa2249db1de
Ludvig Larsson <ludviglarsson@LudvigLaMBP.local>
2019-07-05 17:03:42 +0200

    Added MultiPlot functions, fixed warning messages

--- R/image_processing.R
+++ R/image_processing.R
@@ -66,20 +66,21 @@ LoadImages <- function(
     image_scale(imgs[[i]], paste0(xdim))
   }), nm = samplenames)
 
-  object@tools$pointers <- imgs
-  object@tools$rasters <- setNames(lapply(lapply(seq_along(imgs), function(i) {image_annotate(imgs[[i]], text = samplenames[i], size = round(xdim/10))}), as.raster), nm = samplenames)
+  object@tools$raw <- lapply(imgs, as.raster)
   object@tools$xdim <- xdim
 
   return(object)
 }
 
 
+# TODO: label raster images, why are images black when put into viewer? Compare with warpimages return ...
 
 #' Function used to plot HE images obtained with \code{\link{LoadImages}}
 #'
 #' @param object Seurat object
 #' @param index Image index
-#' @param method Specify display method (raster or viewer)
+#' @param type Specify which image to display [options: "raw", "masked" or "processed"]
+#' @param method Specify display method (raster or viewer).
 #' @param ncols Number of columns in output grid of images
 #' @inheritParams LoadImages
 #'
@@ -90,57 +91,56 @@ LoadImages <- function(
 ImagePlot <- function(
   object,
   index = NULL,
+  type = NULL,
   method = "viewer",
   ncols = NULL
 ) {
 
-  if (!"pointers" %in% names(object@tools)) {
-    stop(paste0("Image paths are not present in Seurat object. Run LoadImages() before plotting."))
+  if (!is.null(type)) {
+    if (!type %in% names(object@tools)) stop(paste0("Invalid type ", type), call. = FALSE)
   }
 
-  images <- object@tools$pointers
-  check_pointers <- any(sapply(lapply(images, function(images) {
-    try(image_info(images))
-  }), class) == "try-error")
-
-  if (check_pointers) {
-    warning(paste0("Image pointer is dead. You cannot save or cache image objects between R sessions. \n",
-                   "Rerun ImageRead if you want to set the image sizes manually. \n",
-                   "Setting image size to '", object@tools$xdim, "' pixels "), call. = F)
-    images <- c()
-    for (path in object@tools$imgs) {
-      images <- c(images, image_read(path))
-    }
-    images <- setNames(lapply(seq_along(images), function(i) {
-      image_scale(images[[i]], paste0(xdim))
-    }), nm = names(images))
-    object@tools$pointers <- images
+  type <- type %||% {
+    choices <- c("processed", "masked", "raw", "processed.masks", "masked.masks")
+    choices[min(as.integer(na.omit(match(names(object@tools), choices))))]
   }
+
+  images <- object@tools[[type]]
 
   if (is.null(index)) {
     ncols <- ncols %||% round(sqrt(length(x = images)))
     nrows <- round(length(x = images)/ncols)
-    stack <- c()
-    for (i in 1:nrows) {
-      i <- i - 1
-      stack <- c(stack, image_append(Reduce(c, images[(i*ncols + 1):(i*ncols + ncols)])))
-    }
+    if (method == "viewer") {
+      images <- lapply(images, image_read)
+      images <- setNames(lapply(seq_along(images ), function(i) {image_annotate(images[[i]], text = names(images)[i], size = round(object@tools$xdim/10))}), nm = names(images))
+      stack <- c()
+      for (i in 1:nrows) {
+        i <- i - 1
+        stack <- c(stack, image_append(Reduce(c, images[(i*ncols + 1):(i*ncols + ncols)])))
+      }
 
-    final_img <- image_append(Reduce(c, stack), stack = T)
+      final_img <- image_append(Reduce(c, stack), stack = T)
+      print(final_img)
+    } else if (method == "raster"){
+      par(mar = c(0, 0, 0, 0), mfrow = c(nrows, ncols))
+      for (rst in images) {
+        plot(rst)
+      }
+    } else {
+      stop(paste0("Invalid display method: ", method), call. = F)
+    }
   } else {
     if (!index %in% 1:length(images)) stop("Image index out of bounds", call. = F)
-    final_img <-images[[index]]
+    final_img <- images[[index]]
+    if (method == "viewer") {
+      final_img <- image_read(final_img)
+      print(final_img)
+    } else if (method == "raster"){
+      plot(final_img)
+    } else {
+      stop(paste0("Invalid display method: ", method), call. = F)
+    }
   }
-
-  if (method == "raster") {
-    plot(as.raster(final_img))
-  } else if (method == "viewer") {
-    print(final_img)
-  } else {
-    stop("Invalid display method", call. = F)
-  }
-
-  return(object)
 }
 
 
@@ -162,7 +162,7 @@ ImagePlot <- function(
 #'
 #' @inheritParams slic
 #'
-#' @importFrom imager magick2cimg medianblur sRGBtoLab as.cimg split_connected
+#' @importFrom imager magick2cimg medianblur sRGBtoLab as.cimg split_connected add
 #' @importFrom dplyr select
 #' @importFrom magrittr %>%
 #' @importFrom stats kmeans
@@ -179,8 +179,13 @@ MaskImages <- function(
   verbose = FALSE
 ) {
 
-  object@tools$rasters <- setNames(lapply(seq_along(object@tools$pointers), function(i) {
-    im <- magick2cimg(object@tools$pointers[[i]]) %>% medianblur(median.blur)
+  rasters <- list()
+  masks <- list()
+  centers <- list()
+
+  for (i in seq_along(object@tools$raw)) {
+    im <- image_read(object@tools$raw[[i]])
+    im <- magick2cimg(im) %>% medianblur(median.blur)
     f <- ecdf(im)
     im <- f(im) %>% as.cimg(dim = dim(im))
     if (verbose) {
@@ -189,7 +194,7 @@ MaskImages <- function(
     }
     out <- slic(im, nS = object@tools$xdim*1.5, compactness)
 
-    d <- sRGBtoLab(out$sp) %>% as.data.frame(wide = "c") %>%
+    d <- sRGBtoLab(out) %>% as.data.frame(wide = "c") %>%
       select(-x,-y)
 
     km <- kmeans(d, 2)
@@ -200,9 +205,9 @@ MaskImages <- function(
     sp <- split_connected(px)
 
     # Check object sizes
-    size.check <- max(unlist(lapply(sp, function(x) sum(x > 0))))/length(seg) > object.size.th
+    size.check <- ifelse(length(sp) > 0, max(unlist(lapply(sp, function(x) sum(x > 0))))/length(seg) > object.size.th, FALSE)
 
-    if (length(sp) == 0 | !size.check) {
+    if (!size.check) {
       seg <- 1 - seg
       px <- seg > 0.5
       sp <- split_connected(px)
@@ -210,15 +215,17 @@ MaskImages <- function(
 
     # Colect pixel coordinates for spots in meta.data slot
     dims.raw <- as.numeric(object@tools$dims[[i]][2:3])
-    dims.scaled <- dim(object@tools$rasters[[i]])
+    dims.scaled <- dim(object@tools$raw[[i]])
     sf.xy <- dims.raw/dims.scaled
     pixel_xy <- sapply(subset(object[[]], sample == paste0(i))[, c("pixel_x", "pixel_y")]/sf.xy, round)
     pixel_coords <- paste(pixel_xy[, 1], pixel_xy[, 2], sep = "x")
 
     # Select pixelsets overlapping with pixel coordinates in meta.data slot
     inds_inside_tissue <- data.frame()
+    keep.sp <- list()
 
-    inds_inside_tissue <- do.call(rbind, lapply(sp, function(px) {
+    for (j in seq_along(sp)) {
+      px <- sp[[j]]
       m <- px[, ]; colnames(m) <- 1:ncol(m); rownames(m) <- 1:nrow(m)
 
       inds <- data.frame(label = as.numeric(m),
@@ -229,21 +236,48 @@ MaskImages <- function(
 
       inds_coords <- paste(inds.under.tissue[, "x.id"], inds.under.tissue[, "y.id"], sep = "x")
       if (length(intersect(pixel_coords, inds_coords)) > 0) {
-        return(subset(inds, label == 0))
+        inds_inside_tissue <- rbind(inds_inside_tissue, subset(inds, label == 0))
+        keep.sp <- c(keep.sp, list(px))
       } else {
-        return(NULL)
+        next
       }
-    }))
+    }
+
+    masks[[i]] <- as.raster(add(keep.sp))
+    #inds_inside_tissue <- do.call(rbind, lapply(sp, function(px) {
+    #  m <- px[, ]; colnames(m) <- 1:ncol(m); rownames(m) <- 1:nrow(m)
+#
+    #  inds <- data.frame(label = as.numeric(m),
+    #                     y.id = rep(1:nrow(m), ncol(m)),
+    #                     x.id = rep(1:ncol(m), each = nrow(m)))
+    #  inds$idx <- 1:nrow(inds)
+    #  inds.under.tissue <- subset(inds, label == 1)
+#
+    #  inds_coords <- paste(inds.under.tissue[, "x.id"], inds.under.tissue[, "y.id"], sep = "x")
+    #  if (length(intersect(pixel_coords, inds_coords)) > 0) {
+    #    return(subset(inds, label == 0))
+    #  } else {
+    #    return(NULL)
+    #  }
+    #}))
 
     if (verbose) {
       cat(paste0("Masking background in image ", i , "\n"))
     }
 
-    rst <- t(object@tools$rasters[[i]])
+    rst <- t(object@tools$raw[[i]])
     rst[inds_inside_tissue$idx] <- "00000000"
     rst <- t(rst)
-    return(rst)
-  }), nm = names(object@tools$rasters))
+    rasters[[i]] <- rst
+
+    # Find tissue center
+    center <- which(seg[, , 1, 1] > 0, arr.ind = T) %>% as.data.frame() %>% summarize(center.x = median(col), center.y = median(row))
+    centers[[i]] <- center
+  }
+
+  object@tools$masked <- setNames(rasters, nm = names(object@tools$raw))
+  object@tools$masked.masks <- setNames(masks, nm = names(object@tools$raw))
+  object@tools$centers <- setNames(centers, nm = names(object@tools$raw))
 
   return(object)
 }
@@ -281,14 +315,12 @@ slic <- function(
   rat <- (sc.spat/sc.col)/(compactness*10)
 
 
-  X <- as.data.frame(im*rat, wide="c") %>% as.matrix
+  X <- as.data.frame(im*rat, wide = "c") %>% as.matrix
   #Generate initial centers from a grid
-  ind <- round(seq(1, nPix(im)/spectrum(im),l=nS))
+  ind <- round(seq(1, nPix(im)/spectrum(im), l = nS))
   #Run k-means
-  km <- kmeans(X, X[ind, ], ...)
+  km <- suppressWarnings({kmeans(X, X[ind, ], ...)})
 
-  #Return segmentation as image (pixel values index cluster)
-  seg <- as.cimg(km$cluster,dim=c(dim(im)[1:2], 1, 1))
   #Superpixel image: each pixel is given the colour of the superpixel it belongs to
   sp <- map(1:spectrum(im),~ km$centers[km$cluster, 2+.]) %>% do.call(c, .) %>% as.cimg(dim = dim(im))
   #Correct for ratio
@@ -298,5 +330,162 @@ slic <- function(
     #Convert back to RGB
     sp <- LabtosRGB(sp)
   }
-  list(km = km, seg = seg, sp = sp)
+
+  return(sp)
 }
+
+
+
+#' Create affine transformation function
+#'
+#' Creates a function that takes x,y values as input and applies a transformation
+#'
+#' @param center.x,center.y Center tissue along x/y axis
+#' @param angle Angle for clock wise rotation given in degrees
+#' @param mirror.x,mirror.y Logical specifying if image should be mirrored along x/y axis
+#' @param dims Raw image dimensions
+#' @param centers Centroid coordinates for tissue
+#'
+
+generate.map.rot <- function (
+  center.x = FALSE,
+  center.y = FALSE,
+  angle = 0,
+  mirror.x = FALSE,
+  mirror.y = FALSE,
+  dims = NULL,
+  centers = NULL
+) {
+  theta <- pi*(angle/360)
+  cx <- ifelse(center.x, as.numeric(centers[1]), dims[1]/2)
+  cy <- ifelse(center.y, as.numeric(centers[2]), dims[2]/2)
+
+  map.rot <- function(
+    x,
+    y
+  ) {
+
+    if(!is.null(dims) & mirror.x) {
+      x <- dims[1] - x; cx <- dims[1] - cx
+    }
+    if(!is.null(dims) & mirror.y) {
+      y <- dims[2] - y; cy <- dims[2] - cy
+    }
+
+    x <- x - cx + dims[1]/2; cx <- dims[1]/2
+    x <- x - cy + dims[2]/2; cy <- dims[2]/2
+    xnew = cos(theta)*(x - cx) - sin(theta)*(y - cy)
+    ynew = sin(theta)*(x - cx) + cos(theta)*(y - cy)
+    list(x = cx + xnew, y = cy + ynew)
+  }
+
+  return(map.rot)
+}
+
+#' Function used to add whitespace to image
+#'
+#' This function adds whitespace to a raster image and forces the height/width
+#' to be equal to the image diagonal. This way you can ensure that no part of the tissue
+#' is cropped after a rotation transformation
+#'
+#' @param rst Raster image
+
+add.margins <- function(
+  rst
+) {
+  d <- round(sqrt(ncol(rst)^2 + nrow(rst)^2))
+  mar.x <- round((d - ncol(rst))/2); mar.y <- round((d - nrow(rst))/2)
+  m <- as.raster(matrix(data = "#FFFFFF", nrow = d, ncol = d))
+  m[(mar.y + 1):(mar.y + nrow(rst)), (mar.x + 1):(mar.x + ncol(rst))] <- rst
+  m <- t(m)
+}
+
+
+#' Apply warping of x, y coordinates using a affine transformation function
+#'
+#' @param im Raster image
+#' @param map.rot Affine transformation function, see \link{\code{generate.map.rot}}
+
+Warp <- function(
+  im,
+  map.rot
+) {
+  copy.im <- as.raster(matrix(data = "#FFFFFF", nrow = nrow(im), ncol = ncol(im)))
+  copy.im <- imwarp(as.cimg(copy.im), map = map.rot, direction = "backward", interpolation = "cubic")
+  inds <- which(copy.im != 255)
+  im <- imwarp(as.cimg(im), map = map.rot, direction = "backward", interpolation = "cubic")
+  #im[inds] <- 255
+  imrst <- as.raster(im)
+  tab.im <- table(imrst)
+  if (length(tab.im) > 2) {
+    im[inds] <- 255
+    imrst <- as.raster(im)
+    imrst[imrst == names(which.max(tab.im))] <- "#FFFFFF"
+  }
+  return(imrst)
+}
+
+# TODO: give examples, fix transformation of pixel coordinates
+
+#' Warps images using various transformations
+#'
+#' @param object Seurat object
+#' @param transforms List of arguments passed to warp function
+#'
+#' @return Seurat object with processed imaged
+#'
+#' @export
+
+WarpImages <- function (
+  object,
+  transforms
+) {
+  if (!"masked" %in% names(object@tools)) stop(paste0("Masked images are not present in Seurat object"), call. = FALSE)
+
+  if (!all(names(transforms) %in% names(object@tools$masked))) stop(paste0("transforms does not match the image labels"))
+
+  warp.functions <- lapply(1:length(object@tools$masked), function(i) NULL)
+  processed.images <- object@tools$masked
+  masks <- object@tools$masked.masks
+  processed.masks <- object@tools$masked.masks
+  warped_coords <- object[[c("pixel_x", "pixel_y")]]
+
+  for (i in names(transforms)) {
+    m <- object@tools$masked[[i]]
+    #m <- add.margins(rst)
+
+    args <- transforms[[i]]
+    center.x <- args[["center.x"]] %||% FALSE
+    center.y <- args[["center.y"]] %||% FALSE
+    angle <- args[["angle"]] %||% 0
+    mirror.x <- args[["mirror.x"]] %||% FALSE
+    mirror.y <- args[["mirror.y"]] %||% FALSE
+
+    #map.rot <- generate.map.rot(center.x, center.y, angle, mirror.x, mirror.y, dims = dim(m), centers = object@tools$centers[[i]] + (dim(m) - dim(rst))/2)
+    map.rot <- generate.map.rot(center.x, center.y, angle, mirror.x, mirror.y, dims = dim(m), centers = object@tools$centers[[i]])
+
+    # Obtain scale factors
+    dims.raw <- as.numeric(object@tools$dims[[i]][2:3])
+    dims.scaled <- dim(object@tools$raw[[i]])
+    sf.xy <- dims.raw/dims.scaled
+    pixel_xy <- subset(object[[]], sample == paste0(i))[, c("pixel_x", "pixel_y")]/sf.xy
+
+    # Warp pixels
+    warped_xy <- sapply(setNames(as.data.frame(do.call(cbind, map.rot(pixel_xy$pixel_x, pixel_xy$pixel_y))), nm = c("warped_x", "warped_y"))*sf.xy, round, digits = 1)
+    warped_coords[rownames(pixel_xy), ] <- warped_xy
+
+    warp.functions[[i]] <- map.rot
+    processed.images[[i]] <- Warp(m, map.rot)
+    msk <- masks[[i]]
+    processed.masks[[i]] <- Warp(msk, map.rot)
+  }
+
+  object@tools$processed <- processed.images
+  object@tools$warp.functions <- warp.functions
+  object@tools$processed.masks <- processed.masks
+  object[[c("warped_x", "warped_y")]] <- warped_coords
+  return(object)
+}
+
+
+
