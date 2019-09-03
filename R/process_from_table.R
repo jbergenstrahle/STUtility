82812c01e37b6cd7a7f0719d50070eabec4cd1df
Ludvig Larsson <ludvig.larsson@scilifelab.se>
2019-09-03 16:43:46 +0200

    Merged with master

--- R/process_from_table.R
+++ R/process_from_table.R
@@ -173,12 +173,10 @@ prep.from.table <- function(
     m@misc$spotfile = FALSE
   }else{m@misc$spotfile = TRUE
   m@tools <- list(imgs = infotable$imgs)}
-<<<<<<< HEAD
-=======
+
   #Add info for manual annotation tool:
   m@meta.data$id <- seq(1:dim(m)[2])
   m@meta.data$labels <- "Default"
->>>>>>> master
 
   cat(paste("After filtering the dimensions of the experiment is: "))
   print(dim(m))
